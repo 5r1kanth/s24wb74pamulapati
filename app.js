89d2acfcfd872413fdb7d93e19a0f256913f27dc
5r1kanth <S560174@nwmissouri.edu>
2024-03-26 17:18:24 -0500

    Completed Assignment

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dragonEggsRouter = require('./routes/dragonEggs');
 var gridRouter = require('./routes/grid');
-var randomItemRouter = require('./routes/pick');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -26,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/dragonEggs', dragonEggsRouter);
 app.use('/grid', gridRouter);
-app.use('/randomitem', randomItemRouter);
+app.use('/pick', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
